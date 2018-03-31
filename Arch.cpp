138f753d4585ed99888b6659eef4047baf4fafe5
Oluwatosin Alagbe <tosinfab52@yahoo.com>
2018-03-31 04:31:44 +0100

    Create Arch.cpp
    Special arc

---
+++ Arch.cpp
@@ -0,0 +1,23 @@
+struct Arch :Shape
+{
+	Arch(Point p, int w, int h, double ang)
+		:w(w), h(h), a(ang)
+	{
+		add(Point(p.x - w, p.y - h));
+	}
+
+	void draw_lines() const;
+
+private:
+	int w;
+	int h;
+	double a;
+
+};
+
+void Arch::draw_lines() const
+{
+	if (color().visibility())
+		fl_arc(point(0).x, point(0).y, w + w, h + h, 0, a);
+
+}
